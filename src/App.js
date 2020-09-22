6abdae1e7aecbbf29191f98d202a19684b977cbf
Lorn Kalb <wahyudwi8514@gmail.com>
2020-09-22 12:22:10 +0300

    Order Button Fineshed

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ class App extends Component {
     purchasable: false
   }
 
+    updatePurchaseState(ingredients) {
+      const sum = Object.keys(ingredients)
+      .map(IgKey => {
+        return ingredients[IgKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchasable: sum > 0 })
+  }
+
   addColorHadler = (type) => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -35,20 +46,7 @@ class App extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-  }
-
-  updatePurchaseState() {
-    const ingredients = {
-      ...this.state.ingredients
-    };
-    const sum = Object.keys(ingredients)
-      .map(IgKey => {
-        return ingredients[IgKey];
-      })
-      .reduce((sum, el) => {
-        return sum + el;
-      }, 0);
-    this.setState({ purchasable: sum > 0 })
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeColorHadler = (type) => {
@@ -65,6 +63,7 @@ class App extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
 
   }
 
@@ -86,7 +85,7 @@ class App extends Component {
               colorAdded={this.addColorHadler}
               colorRemoved={this.removeColorHadler}
               disabled={disableInfo}
-              purchasable={this.updatePurchaseState}
+              purchasable={this.state.purchasable}
               price={this.state.totalPrice} />
           </div>
           <div className={classes.Text}>
