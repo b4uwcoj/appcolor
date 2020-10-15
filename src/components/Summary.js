11843ace79041584ea81181426a958b052ef0cb9
b4uwcoj <wahyudwi8514@gmail.com>
2020-10-15 14:46:53 +0300

    shouldComponentUpdate & Axios setup

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../hoc/Auxilary';
 import Button from './Button';
 
-const summary = ( props ) => {
-    const ingredientSummary = Object.keys( props.ingredients )
+class Summary extends Component {
+    componentDidUpdate () {
+        console.log('[Summary] DidUpdate');
+    }
+
+    render () {
+            const ingredientSummary = Object.keys( this.props.ingredients )
         .map( igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li> );
         } );
 
-    return (
-        <Aux>
+        return (
+                    <Aux>
             <h3>Your Colors</h3>
             <p>A burger color</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancled}>Cancle</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancled}>Cancle</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
-};
+        );
+    }
+}
 
-export default summary;
+export default Summary;
 
